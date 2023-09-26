cd92ceb97901e0aa08d006fd6d1698b24d96ecf4
GhostDaGhost <53021768+GhostDaGhost@users.noreply.github.com>
2023-09-26 16:01:29 -0700

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+    * @title Hello World
+    * @description The first Java script I've ever built... literally.
+    * @author Alexander Arizola
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
